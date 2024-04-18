ccacbfa5db1913954448bf1432927f3fc48f7530
Rishabh Tripathi <devr1310@gmail.com>
2024-04-18 19:09:21 +0530

    added tinycloud api key

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Editor } from '@tinymce/tinymce-react';
 import { Controller } from 'react-hook-form';
-
+import conf from '../conf/conf.js';
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
     return (
@@ -13,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
-                        apiKey='zgftmhelz8c9t4psbn88i9wzo7vn49t1zb7yopy6lhlxwaq9'
+                        apiKey={conf.tinycloudApiKey}
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
